c89ec1c1f087de3a7ce8751f1a7dbeb19c0603a7
ananthr99 <ananth.r@invendis.com>
2026-09-22 15:58:24 +0530

    Fix cms-admin navigation and GitHub Pages refresh issue

--- apps/cms-admin/src/main.jsx
+++ apps/cms-admin/src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import AuthProvider from "./auth/AuthProvider.jsx";
 import { AdminProvider } from "./context/AdminContext.jsx";
 import App from "./App.jsx";
@@ -9,11 +9,11 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<AuthProvider>
-			<BrowserRouter basename={import.meta.env.BASE_URL}>
+			<HashRouter basename={import.meta.env.BASE_URL}>
 				<AdminProvider>
 					<App />
 				</AdminProvider>
-			</BrowserRouter>
+			</HashRouter>
 		</AuthProvider>
 	</React.StrictMode>
 );
